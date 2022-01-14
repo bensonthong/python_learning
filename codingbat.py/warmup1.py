ae57c42d731c0c3a8b2fcafcf77829a43dc83ec8
BenDaSon <66227475+BenDaSon@users.noreply.github.com>
2022-01-13 18:58:40 -0800

    Update warmup1.py

--- codingbat.py/warmup1.py
+++ codingbat.py/warmup1.py
@@ -21,3 +21,36 @@ def diff21(n):
     return 2*abs(2-n)
   else:
     return 21-n
+
+
+#Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute value of a number.
+#near_hundred(93) → True
+#near_hundred(90) → True
+#near_hundred(89) → False
+
+  def near_hundred(n):
+  withinHundred = 100-n
+  withinTwoHundred = 200-n
+  
+  if abs(withinHundred) > 10 and abs(withinTwoHundred) > 10:
+    return False
+  return True
+
+def near_hundred(n):
+  if abs(100-n)<=10 or abs(200-n)<=10:
+    return True
+  return False
+
+#Given a non-empty string and an int n, return a new string where the char at index n has been removed. 
+#The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+#missing_char('kitten', 1) → 'ktten'
+#missing_char('kitten', 0) → 'itten'
+#missing_char('kitten', 4) → 'kittn'
+
+def missing_char(str, n):
+  return str.replace(str[n],"")
+
+def missing_char(str, n):
+  front = str[:n]   # up to but not including n
+  back = str[n+1:]  # n+1 through end of string
+  return front + back
